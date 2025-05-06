832d348c2c48e568e73c4d9f1b7adaac353c0db9
Andres Rangel <andresrangel8a@gmail.com>
2025-05-06 13:11:49 -0500

    Arreglo General

--- backend/src/controllers/categorias.controllers.js
+++ backend/src/controllers/categorias.controllers.js
@@ -3,10 +3,8 @@ import getConnection from "../db/database.js";
 const getCamisetas = async (req, res) => {
   try {
     const connection = await getConnection();
-    const result = await connection.query(
-      "SELECT * FROM camisetas"
-    );
-    res.json(result);
+    const [rows] = await connection.query("SELECT * FROM camisetas");
+    res.json(rows);
   } catch (error) {
     console.error("Error al obtener las camisetas:", error);
     res.status(500).send("Error en el servidor");
@@ -15,4 +13,4 @@ const getCamisetas = async (req, res) => {
 
 export const methodHTTP = {
   getCamisetas,
-}
+};
