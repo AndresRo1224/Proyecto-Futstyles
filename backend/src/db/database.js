c38e9968958f0681f688fcb09686f66704b911ee
Andres Rangel <andresrangel8a@gmail.com>
2025-05-06 13:30:04 -0500

    Intento de conexion

--- backend/src/db/database.js
+++ backend/src/db/database.js
@@ -1,15 +1,17 @@
 import mysql from "mysql2/promise";
-import config from "../config.js";
+import dotenv from "dotenv";
+
+dotenv.config();  // Carga las variables del archivo .env
 
 let connection;
 
 const getConnection = async () => {
   if (!connection) {
     connection = await mysql.createConnection({
-      host: config.host,
-      user: config.user,
-      password: config.password,
-      database: config.database,
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     });
     console.log("Conectado a la base de datos");
   }
